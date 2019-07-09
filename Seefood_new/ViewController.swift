10653052e59e3480a5b7c66c1c5515237d57dcb8
Davide Santo <ing.davide.santo@gmail.com>
2019-07-09 12:05:27 +0200

    Initial Commit

---
+++ Seefood_new/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Seefood_new
+//
+//  Created by Davide Santo on 09.07.19.
+//  Copyright © 2019 Davide Santo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
